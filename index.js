9ffe4f6f2a55652a0e87069aa56cc8adafb4d8bb
Ken Sin <kensin1129@gmail.com>
2015-05-12 14:59:06 -0400

    Push to kickstart travis

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 
 module.exports = {
   name: 'ember-keen-tracking',
-
   contentFor: function(type) {
     if(type === 'head') {
       return '<script src="https://d26b395fwzu5fz.cloudfront.net/3.2.4/keen-tracker.min.js" type="text/javascript"></script>';
