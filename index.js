8a7a8080fbd202022f70fc1d9ddfdf58ab1249fe
Ken Sin <kensin1129@gmail.com>
2015-05-21 12:04:37 -0400

    Update include hook

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 /* jshint node: true */
 'use strict';
+var path = require('path');
 
 module.exports = {
   name: 'ember-keen-tracking',
+
+  blueprintsPath: function() {
+    return path.join(__dirname, 'blueprints');
+  },
+
   included: function(app) {
-    this._super.included(app);
-    app.import(app.bowerDirectory + '/keen-js/dist/keen-tracker.js');
+    this._super.included.apply(this, arguments);
+
+    var keenPath = path.join(app.bowerDirectory, '/keen-js/dist');
+
+    app.import(path.join(keenPath, 'keen-tracker.js'));
   }
 };
